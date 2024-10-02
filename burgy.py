4ba926708d02d75534d22eaa469a8309fc0c4552
Stigstille <134349048+Stigstille@users.noreply.github.com>
2024-10-02 12:12:20 -0400

    Add Daily Burgy Stats to webhook

--- burgy.py
+++ burgy.py
@@ -5,7 +5,7 @@ import pygsheets
 
 from findBurgy import *
 
-version = "1.0.4"
+version = "1.0.5"
 url="DISCORDWEBHOOKURL" # NEVER PUBLISH THIS!!!!
 
 if len(sys.argv) != 2:
@@ -38,6 +38,10 @@ data = {
 }
 requests.post(url, json=data)
 wks.cell('A1').note = f"Using Burgy Uploader v{version}"
+newBurgys = 0
+totalBurgys = 0
+newUsers = 0
+totalUsers = 0
 try:
     lines = open(sys.argv[1]).read().splitlines()
     for line in lines:
@@ -46,6 +50,7 @@ try:
         if line == "------ NEW DAY ------":
             print("----STARTING NEW DAY----")
         else:
+            totalBurgys += 1
             divLine = line.split(" got ")
             divLine[1] = divLine[1].split("!")[0]
             print(divLine)
@@ -55,12 +60,17 @@ try:
                 for cell in usrnms:
                     cellData = [cell[0].label, cell[0].value]
                     if (cellData[1].lower() == divLine[0].lower()) and not found:
-                        data["content"] = f"{divLine[0]} now has {findBurgy(divLine, cellData, wks, burgyNames)} {divLine[1]}!!"
+                        foundBurgy = findBurgy(divLine, cellData, wks, burgyNames)
+                        data["content"] = f"{divLine[0]} now has {foundBurgy} {divLine[1]}!!"
+                        if foundBurgy == "their first":
+                            newBurgys += 1
                         requests.post(url, json=data)
                         found = True
                     elif (cellData[1] == "") and not found:
                         if not madeNew:
                             if cellData[0] != "A3":
+                                newUsers += 1
+                                newBurgys += 1
                                 wks.update_value(f'{cellData[0]}', divLine[0])
                                 madeNew = True
                                 data["content"] = f"# NEW USER!!\n{divLine[0]} now has {findBurgy(divLine, cellData, wks, burgyNames)} {divLine[1]}!!"
@@ -72,7 +82,13 @@ try:
                 print(f"This burgy ({divLine[1]}) was triggered manually (10 Day Streak)")
                 data["content"] = f"There was a burgy ({divLine[1]}) that was triggered manually (10 Day Streak)"
                 requests.post(url, json=data)
-
+    usrnmsFinal = wks.range('A:A')
+    for cell in usrnmsFinal:
+        cellData = [cell[0].label, cell[0].value]
+        if (not cellData[0] in ["A1", "A2", "A3"]) and (not cellData[1] == ""):
+            totalUsers += 1
+    data["content"] = f"<:BURGYBUTCLOSEUP:1202870909178875985> DAILY BURGY STATS <:BURGYBUTCLOSEUP:1202870909178875985> \nNew Burgies rolled today: {newBurgys}\nFirst Time Burgy rollers: {newUsers}\nTotal Burgies rolled: {totalBurgys}\nTotal Burgy Enjoyers: {totalUsers}\nBurgy uploader created by: Stigstille"
+    requests.post(url, json=data)
 except IOError:
     print("Could not read file:", sys.arv[1])
 except Exception as ex:
